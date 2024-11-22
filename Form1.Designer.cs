69f6a3df57b557e69c587763a3fccfb175e381a5
Not-Callum <callummarkjones@gmail.com>
2024-11-22 14:23:25 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CRUDProject
+{
+    partial class CRUDTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.TodaysHealthBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.DateLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.Color.PaleGreen;
+            this.SearchButton.Location = new System.Drawing.Point(415, 43);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 0;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGridView.BackgroundColor = System.Drawing.Color.LightGray;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(12, 72);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(776, 336);
+            this.DataGridView.TabIndex = 1;
+            this.DataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_CellClick);
+            this.DataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_CellContentClick);
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(52, 43);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(357, 20);
+            this.SearchTextBox.TabIndex = 2;
+            this.SearchTextBox.TextChanged += new System.EventHandler(this.SearchTextBox_TextChanged);
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.BackColor = System.Drawing.Color.PaleGreen;
+            this.InsertButton.Location = new System.Drawing.Point(550, 43);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(75, 23);
+            this.InsertButton.TabIndex = 3;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = false;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.BackColor = System.Drawing.Color.PaleGreen;
+            this.UpdateButton.Location = new System.Drawing.Point(631, 43);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 4;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = false;
+            this.UpdateButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.IndianRed;
+            this.Delete.Location = new System.Drawing.Point(712, 43);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 5;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // TodaysHealthBar
+            // 
+            this.TodaysHealthBar.BackColor = System.Drawing.Color.SlateGray;
+            this.TodaysHealthBar.ForeColor = System.Drawing.Color.Red;
+            this.TodaysHealthBar.Location = new System.Drawing.Point(147, 414);
+            this.TodaysHealthBar.Name = "TodaysHealthBar";
+            this.TodaysHealthBar.Size = new System.Drawing.Size(539, 25);
+            this.TodaysHealthBar.TabIndex = 6;
+            this.TodaysHealthBar.Value = 50;
+            this.TodaysHealthBar.Click += new System.EventHandler(this.TodaysHealthBar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Task:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Location = new System.Drawing.Point(693, 18);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(35, 13);
+            this.DateLabel.TabIndex = 10;
+            this.DateLabel.Text = "label2";
+            this.DateLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // CRUDTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(799, 451);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TodaysHealthBar);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.SearchButton);
+            this.Name = "CRUDTest";
+            this.Text = "To-Do 2 Ta-Da";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.ProgressBar TodaysHealthBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label DateLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CRUDProject
+{
+
+    public partial class CRUDTest : Form
+    {
+
+        DateTime today = DateTime.Today;
+        public CRUDTest()
+        {
+            InitializeComponent();
+
+            Check_Tasks_Today();
+            update_Date_Label();
+            task_Completion_Bar_Update();
+            read_Data_Table_Date();
+
+        }
+
+        SqlConnection con = new SqlConnection(@"Your-Connection-String-Here");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            if (SearchTextBox.Text != "")
+            {
+                DateTime selectedTime = today;
+                string dateString = selectedTime.ToString("yyyy-MM-dd");
+                MessageBox.Show(dateString);
+
+                using (SqlCommand cmd = new SqlCommand("dbo.Task_Search", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Task", SearchTextBox.Text);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    DataGridView.DataSource = dataTable;
+                }
+
+
+            }
+            else
+            {
+                DateTime selectedTime = today;
+                string dateString = selectedTime.ToString("yyyy-MM-dd");
+                MessageBox.Show(dateString);
+
+                SqlCommand cmd = new SqlCommand("dbo.Date_Search", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@TaskDate", dateString);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                DataGridView.DataSource = dataTable;
+
+            }
+            con.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DateTime selectedTime = today;
+            string dateString = selectedTime.ToString("yyyy-MM-dd");
+            int rows = DataGridView.RowCount - 1;
+
+            MessageBox.Show(rows.ToString());
+            for (int i = 0; i < rows; i++)
+            {
+                Debug.WriteLine(DataGridView.Rows[i].Cells[0].Value);
+                Debug.WriteLine(DataGridView.Rows[i].Cells[1].Value);
+                Debug.WriteLine(DataGridView.Rows[i].Cells[2].Value);
+
+                string task_update = DataGridView.Rows[i].Cells[0].Value.ToString();
+                string task_completion_update = DataGridView.Rows[i].Cells[1].Value.ToString();
+                string permatask_update = DataGridView.Rows[i].Cells[2].Value.ToString();
+                update_With_Values(task_update, task_completion_update, dateString, permatask_update);
+            }
+
+            task_Completion_Bar_Update();
+        }
+
+
+        private void DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            ////When the date picker changes value >>
+            //DateTime selectedTime = Date.Value;
+            //string dateString = selectedTime.ToString("yyyy-MM-dd");
+            ////MessageBox.Show(dateString);
+            //con.Open();
+            ////create a new command "request"
+            //SqlCommand cmd = new SqlCommand("dbo.Date_Search", con);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            ////Add values to that command
+            //cmd.Parameters.AddWithValue("@TaskDate", dateString);
+            ////Adapt with the output of the command
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //DataGridView.DataSource = dataTable;
+            //con.Close();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (SearchTextBox.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                //would probably make sense to check if the selected task is permatask or not
+                string deltask = SearchTextBox.Text;
+
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("dbo.Task_delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Task", deltask);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                MessageBox.Show("Successfully deleted task!");
+                LoadAllRecords();
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TodaysHealthBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            if (SearchTextBox.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                try
+                {
+                    string newtask = SearchTextBox.Text;
+                    DateTime TodaysDate = DateTime.Today;
+                    string dateString = TodaysDate.ToString("yyyy-MM-dd");
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("dbo.New_Task_Insert", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Task", newtask);
+                    cmd.Parameters.AddWithValue("@TaskCompletion", "False");
+                    cmd.Parameters.AddWithValue("@TaskDate", dateString);
+                    cmd.Parameters.AddWithValue("PermaTask", "False");
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                    MessageBox.Show("Successfully added new task!");
+                    LoadAllRecords();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Check_Tasks_Today()
+        {
+            DateTime TodaysDate = DateTime.Today;
+            string dateString = TodaysDate.ToString("yyyy-MM-dd");
+            //MessageBox.Show(dateString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Date_Search", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@TaskDate", dateString);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            DataGridView.DataSource = dataTable;
+            con.Close();
+        }
+
+        void LoadAllRecords()
+        {
+            SqlCommand cmd = new SqlCommand("dbo.Task_view", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            DataGridView.DataSource = dataTable;
+            task_Completion_Bar_Update();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void update_Date_Label()
+        {
+            DateTime TodaysDate = DateTime.Today;
+            string dateString = TodaysDate.ToString("yyyy-MM-dd");
+            DateLabel.Text = ("Date: " + dateString);
+        }
+
+        private void update_On_Date_Change()
+        {
+            //Delete all tasks that are not "PermaTask"
+            delete_Non_Permanent_Tasks();
+            //Change remaining task_date on tasks to today( replace where TaskDate != Today)
+            change_All_Task_Dates();
+            //Uncomplete them all ( replace where TaskCompletion == true)
+            reset_Task_Completion();
+            LoadAllRecords();
+        }
+
+        void delete_Non_Permanent_Tasks()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Delete_Non_Perma_Tasks", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            DataGridView.DataSource = dataTable;
+            con.Close();
+        }
+
+        void change_All_Task_Dates()
+        {
+            DateTime TodaysDate = DateTime.Today;
+            string dateString = TodaysDate.ToString("yyyy-MM-dd");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Update_Tasks_Date", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Task_Date", dateString);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            DataGridView.DataSource = dataTable;
+            con.Close();
+        }
+
+        void read_Data_Table_Date()
+        {
+            //format date the same way SQLReader does
+            DateTime TodaysDate = DateTime.Today;
+            string dateString = TodaysDate.ToString("dd/MM/yyyy HH:mm:ss");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ToDoTable", con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader DReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            List<string> datesList = new List<string>();
+            while (DReader.Read())
+            {
+                datesList.Add(DReader[2].ToString());
+            }
+
+            con.Close();
+
+
+
+            if (datesList[0] != dateString)
+            {
+                Debug.WriteLine("The dates do not match");
+                Debug.WriteLine(datesList[0] + " " + dateString);
+                update_On_Date_Change();
+                MessageBox.Show("Hello! Welcome to the new day! Hope you're ready to conquer it!");
+            }
+            else
+            {
+                Debug.WriteLine("They do match");
+            }
+        }
+
+        void reset_Task_Completion()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Reset_Task_Completion", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            DataGridView.DataSource = dataTable;
+            con.Close();
+        }
+
+        void update_With_Values(string task, string task_completion, string date, string perma_task)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Task_Update", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Task", task);
+            cmd.Parameters.AddWithValue("@TaskCompletion", task_completion);
+            cmd.Parameters.AddWithValue("@TaskDate", date);
+            cmd.Parameters.AddWithValue("@PermaTask", perma_task);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        void task_Completion_Bar_Update()
+        {
+            int completionNumber = 0;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ToDoTable", con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader DReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            List<string> completionList = new List<string>();
+            while (DReader.Read())
+            {
+                completionList.Add(DReader[1].ToString());
+            }
+
+            con.Close();
+
+            for (int i = 0; i < completionList.Count; i++)
+            {
+                if (completionList[i] == "True")
+                {
+                    completionNumber++;
+                }
+            }
+            Debug.WriteLine("Completed: " + completionNumber);
+            float completionPercentage = ((float)completionNumber / (float)completionList.Count) * 100;
+            Debug.WriteLine(completionPercentage + "%");
+            TodaysHealthBar.Value = (int)completionPercentage;
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView.CurrentRow.Selected = true;
+            SearchTextBox.Text = DataGridView.Rows[e.RowIndex].Cells["Task"].Value.ToString();
+        }
+    }
+}
